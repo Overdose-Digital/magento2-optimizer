72a4d734aec5e88a5693fbbee39c3920dd10218b
Ihor Panchak <ihor.panchak@overdose.digital>
2022-04-25 20:36:13 +0300

    Code refactoring. https://www.wrike.com/open.htm?id=858381046

--- Observer/Frontend/Http/ResponseSendBeforeOptimizeJS.php
+++ Observer/Frontend/Http/ResponseSendBeforeOptimizeJS.php
@@ -2,46 +2,59 @@
 
 namespace Overdose\MagentoOptimizer\Observer\Frontend\Http;
 
+use Magento\Framework\App\Request\Http as RequestHttp;
+use Magento\Framework\App\Response\Http as ResponseHttp;
+use Magento\Framework\Event\ObserverInterface;
 use Overdose\MagentoOptimizer\Helper\Data;
 use Magento\Framework\Event\Observer;
 
-class ResponseSendBeforeOptimizeJS extends AbstractObserver implements \Magento\Framework\Event\ObserverInterface
+/**
+ * Class ResponseSendBeforeOptimizeJS
+ */
+class ResponseSendBeforeOptimizeJS extends AbstractObserver implements ObserverInterface
 {
     const JS_LOOK_FOR_STRING = '#(<script *\b(?!nodefer)\b\S+.*?<\/script>)#is';
-    //const JS_LOOK_FOR_STRING = '#(<script.*?</script>)#is';
 
     /**
      * @param Observer $observer
-     * @return false
+     * @return void
      */
     public function execute(Observer $observer)
     {
-        if ($this->dataHelper->isMoveJsEnabled()) {
-            /** @var $request \Magento\Framework\App\Request\Http */
-            $request = $observer->getEvent()->getRequest();
-            if ($request->isAjax()) {
-                return false;
-            }
-
-            if (!$this->checkControllersIfExcluded($request, Data::KEY_FIELD_EXCLUDE_CONTROLLERS, Data::KEY_SCOPE_MOVE_JS_BOTTOM_PAGE)) {
-                return false;
-            }
-
-            if (!$this->checkPathIfExcluded($request, Data::KEY_FIELD_EXCLUDE_PATH, Data::KEY_SCOPE_MOVE_JS_BOTTOM_PAGE)) {
-                return false;
-            }
-            //move js to the bottom page
-            $this->moveJs($observer);
+        if (!$this->dataHelper->isMoveJsEnabled()) {
+            return;
         }
+
+        /** @var RequestHttp|null $request */
+        $request = $observer->getEvent()->getData('request');
+
+        if ($request->isAjax()
+            || !$this->checkControllersIfExcluded($request, Data::KEY_FIELD_EXCLUDE_CONTROLLERS, Data::KEY_SCOPE_MOVE_JS_BOTTOM_PAGE)
+            || !$this->checkPathIfExcluded($request, Data::KEY_FIELD_EXCLUDE_PATH, Data::KEY_SCOPE_MOVE_JS_BOTTOM_PAGE)
+        ) {
+            return;
+        }
+
+        $this->moveJs($observer);
     }
 
+    /**
+     * @param Observer $observer
+     * @return void
+     */
     public function moveJs(Observer $observer)
     {
+        /** @var ResponseHttp|null $response */
+        $response = $observer->getEvent()->getData('response');
+
+        if (!$response) {
+            return;
+        }
+
         $deferredJs = '';
-        $response = $observer->getEvent()->getResponse();
         $html = preg_replace_callback(
             static::JS_LOOK_FOR_STRING,
-            function ($script) use (&$deferredJs) {
+            static function ($script) use (&$deferredJs) {
                 $deferredJs .= $script[0];
 
                 return '';
